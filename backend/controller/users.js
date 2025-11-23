76081a55a1d5f5ab2f89ac850338b171d0427670
Jaswant Singh <singhjaswant0932@gmail.com>
2025-11-23 13:32:46 +0000

    backend-POST request for add user

--- backend/controller/users.js
+++ backend/controller/users.js
@@ -1,4 +1,5 @@
 const { pool, executeQuery } = require("../utils/db");
+const bcrypt = require("bcrypt");
 const upload = require("../utils/multer");
 const getUsers = async (req, res) => {
   try {
@@ -12,8 +13,45 @@ const getUsers = async (req, res) => {
 
 const addUser = async (req, res) => {
   try {
+    const {
+      user_name,
+      user_address,
+      user_email,
+      user_contact,
+      login_username,
+      password,
+    } = req.body;
+
+    if (!login_username || !password) {
+      return res
+        .status(400)
+        .json({ message: "Username and password required" });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const query = `
+      INSERT INTO users_def 
+      (user_name, user_address, user_email, user_contact, created_date, login_username, password) 
+      VALUES ($1, $2, $3, $4, NOW(), $5, $6)
+      RETURNING user_id, user_name, user_email, login_username
+    `;
+
+    const values = [
+      user_name,
+      user_address,
+      user_email,
+      user_contact,
+      login_username,
+      hashedPassword,
+    ];
+    const result = await executeQuery(query, values);
+    res.status(201).json({
+      message: "User created successfully",
+      admin: result.rows[0],
+    });
   } catch (error) {
     console.log(error);
+    res.status(500).json({ message: "Server Error", success: false });
   }
 };
 
