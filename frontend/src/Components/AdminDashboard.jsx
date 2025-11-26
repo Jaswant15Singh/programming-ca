4deb01acbd203aa4e6aaf5621970c0f9ae034b77
Jaswant Singh <singhjaswant0932@gmail.com>
2025-11-26 14:08:54 +0000

    frontend-Promise.all used to fetch dashboard data

--- frontend/src/Components/AdminDashboard.jsx
+++ frontend/src/Components/AdminDashboard.jsx
@@ -1,41 +1,66 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "../stylesheet/AdminDashboard.css";
 
 function AdminDashboard() {
+  const [users, setUsers] = useState([]);
+  const [zones, setZones] = useState([]);
+  const [officers, setOfficers] = useState([]);
+  const [complaints, setComplaints] = useState([]);
+
+  useEffect(() => {
+    getAllData();
+  }, []);
+
+  async function getAllData() {
+    const result = await Promise.all(
+      [
+        "http://localhost:5000/users/get-users",
+        "http://localhost:5000/complaint/get-complaints",
+        "http://localhost:5000/admin/get-zones",
+        "http://localhost:5000/admin/get-all-officers",
+      ].map(async (e) => {
+        const result = await fetch(e);
+        const data = await result.json();
+        return data;
+      })
+    );
+
+    setUsers(result[0]);
+    setComplaints(result[1]);
+    setZones(result[2]);
+    setOfficers(result[3]);
+  }
   return (
     <div className="dashboard">
       <h1>Dashboard Overview</h1>
-
       {/* Cards */}
       <div className="cards">
         <div className="card">
           <h3>Total Users</h3>
-          <p className="value">1,245</p>
+          <p className="value">{users.length}</p>
         </div>
 
         <div className="card">
-          <h3>Active Orders</h3>
-          <p className="value">312</p>
+          <h3>Zones</h3>
+          <p className="value">{zones.length}</p>
         </div>
 
         <div className="card">
-          <h3>Revenue</h3>
-          <p className="value">$8,540</p>
+          <h3>Officers</h3>
+          <p className="value">{officers.length}</p>
         </div>
 
         <div className="card">
-          <h3>Pending Tasks</h3>
-          <p className="value">19</p>
+          <h3>Complaints</h3>
+          <p className="value">{complaints.length}</p>
         </div>
       </div>
 
-      {/* Chart Section */}
-      <div className="chart-section">
+      {/* <div className="chart-section">
         <div className="chart-box">Chart Placeholder</div>
         <div className="chart-box">Statistics Placeholder</div>
       </div>
 
-      {/* Recent Table */}
       <div className="table-section">
         <h2>Recent Activities</h2>
 
@@ -68,7 +93,7 @@ function AdminDashboard() {
             </tr>
           </tbody>
         </table>
-      </div>
+      </div> */}
     </div>
   );
 }
