f5568d09ae81830b160cceb460283bc092df4f0c
Jaswant Singh <singhjaswant0932@gmail.com>
2025-11-26 11:48:53 +0000

    frontend-Protected route function along with jwt-decode library for decoding jsonwebtoken

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,34 @@
+import { jwtDecode } from "jwt-decode";
 import "./App.css";
 import Home from "./Pages/Home";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import UserLogin from "./Pages/UserLogin";
+import AdminLogin from "./Pages/AdminLogin";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+
+function ProtectedRoute({ children }) {
+  const token = localStorage.getItem("admin-token");
+
+  if (!token) {
+    return <Navigate to="/admin-login" replace />;
+  }
+
+  try {
+    const jwt = jwtDecode(token);
+    console.log(jwt);
+    return children;
+  } catch (e) {
+    console.log(e);
+
+    return <Navigate to="/admin-login" replace />;
+  }
+}
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route exact path="/" element={<Home />} />
-        <Route exact path="/admin-login" element={<>Admin login</>} />
-        <Route exact path="/resident-login" element={<>Resident login</>} />
+        <Route exact path="/admin-login" element={<AdminLogin />} />
+        <Route exact path="/resident-login" element={<UserLogin />} />
         <Route exact path="/admin/dashboard" element={<>Admin Dashboard</>} />
         <Route
           exact
