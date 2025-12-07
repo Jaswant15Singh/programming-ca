fd5a9f7fe29d4815fe69cd537031c4ad83e5338c
Jaswant Singh <singhjaswant0932@gmail.com>
2025-12-07 14:36:01 +0000

    backend-config removed for cloud

--- backend/index.js
+++ backend/index.js
@@ -11,11 +11,7 @@ const officer = require("./router/officer");
 app.use(express.static("public"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-// app.use(express.static(path.join(__dirname, "../frontend/build")));
 
-// app.get("*", (req, res) => {
-//   res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
-// });
 app.use(
   cors({
     origin: "*",
